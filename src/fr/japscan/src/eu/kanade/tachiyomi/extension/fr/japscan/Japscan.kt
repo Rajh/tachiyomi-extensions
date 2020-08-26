dd0118bc3f70a9c43db573eea0f9e37f4880a9c6
Rajh <Hetmoteus@gmail.com>
2020-08-26 22:03:22 +0200

    Ugly fix copied from mangaminu

--- src/fr/japscan/src/eu/kanade/tachiyomi/extension/fr/japscan/Japscan.kt
+++ src/fr/japscan/src/eu/kanade/tachiyomi/extension/fr/japscan/Japscan.kt
@@ -24,6 +24,8 @@ import java.io.InputStream
 import java.text.ParseException
 import java.text.SimpleDateFormat
 import java.util.Locale
+import kotlin.collections.ArrayList
+import kotlin.collections.HashMap
 import okhttp3.FormBody
 import okhttp3.MediaType
 import okhttp3.OkHttpClient
@@ -31,6 +33,8 @@ import okhttp3.Request
 import okhttp3.Response
 import okhttp3.ResponseBody
 import org.apache.commons.lang3.StringUtils
+import org.json.JSONArray
+import org.json.JSONObject
 import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
 
@@ -46,23 +50,8 @@ class Japscan : ParsedHttpSource() {
 
     override val supportsLatest = true
 
-    private val keysheetChapterUrl1 = "https://www.japscan.co/lecture-en-ligne/bloody-kiss/volume-1/"
-    private val keysheetChapterUrl2 = "https://www.japscan.co/lecture-en-ligne/1-nen-a-gumi-no-monster/1/"
-
-    private var keysheet = "0123456789abcdefghijklmnopqrstuvwxyz"
-
-    private val realpageBk2 = "221518b2f9a20842e4c5379223a5881255d2e522c32277923242520284a542f28085b952e422b1e5a97216b262e5e6621535a22/552c5c112f3e2c4f2e122f025517294925042f322d54513e25822c7e2ev0274751116a4266386a0d56w2506a578b562f6exd2e6/65f435e996fs95fw1160125x715062eve104e33va38454bu632x1315b4fxa10244e2c438932ta38v1158d4e323cx237x933u435/1038xa1"
-    private val realpageBk4 = "a336f98370e3d9130516d873045639a35633468374c3389353f36313759623633176406345c3e2e60033e713c37687239616b3f6732612a3d4437/5738213e1d642b3d5a3f113d4e3b666c423e973a8d3bja3a50662772597a4270196akc6b74609a613277l1337d6c0776ge60k3220735l85410640/33a25325d2a927b446al56al36fi8672e4dl120113dj7205543ja455354i847lc436955lf26315739529643h744j6249c5e4e40l34el14ci74822/49l12"
-    private val realpageBk14 = "4033163017801660626355d0118326c0c3202380719055b0e070e020a2e3d0c04883e750d2a029f397704470f0e3a4208303f0b370a37930816022/f0097038e3a9a0e2c0781021d0f303a120f6d075b05nd0e27379d48204b164f8a36od374a3c6f3e0b4fpa0841357f44kc3co798740c8c02p924853/e7b0398052d996e491534p331p331m8319616p6958d01n8922c1cn2122827m915pb1e372ep29a0e2d0c24681el51dna926f291714p71fpe1am2189/01fpb9"
-    private val realpageBk29 = "e952450996d945591172e4b9e0528549828912293089e469a939a939c172c9d997b276f991c9a842566973f96922e359c222f9b21/932984980e931e908d98742f819f179a7a9d0c972e2a07975794419az6931c248032173809377523a028322b51279a3fbf903e2d6/03dw72bae8a6399bd1f7f26bf8d7196za85140fz00d121ey504b0092d14b581971e9b1e5508x408z8815213080eb60ab30ay30c87/0cbd8"
-    private val realpageMonster1 = "d7b023a0f83391903313f630b090311054d04910118397b0c140003368b3e3d343e0a390a76032c0d990f16387104953d110405378d030/4388f0d7a052f05820e853203377a04760e06034a0c5b019c05p805223198472d41174f863aqe394f3e6c32014ar30642387f41m13dqe9/f8a0f7700rf95890era9a9e26211a1a29062dqb12ra1fp319p993r11fq31bp9936115pd1421214819pb9c8410p396qb1e2d14qe1ap99a8/603rc9"
-    private val realPageUrls = listOf(
-        realpageBk2,
-        realpageBk4,
-        realpageBk14,
-        realpageBk29,
-        realpageMonster1
-    )
+    private val dicc: HashMap<String, String> = HashMap()
+    private val currentScripts: ArrayList<String> = ArrayList()
 
     override val client: OkHttpClient = network.cloudflareClient.newBuilder().addInterceptor { chain ->
         val indicator = "&decodeImage"
@@ -91,16 +80,6 @@ class Japscan : ParsedHttpSource() {
         }
     }
 
-    private fun loadKeysheetChapters() {
-        var response = client.newCall(GET(keysheetChapterUrl1, headers)).execute()
-        val doc = response.asJsoup()
-        response = client.newCall(GET(keysheetChapterUrl2, headers)).execute()
-        val doc2 = response.asJsoup()
-
-        // 「MULTI-DOCUMENT DRIFTING!!」
-        createKeysheet(doc, doc2)
-    }
-
     // Popular
     override fun popularMangaRequest(page: Int): Request {
         return GET("$baseUrl/mangas/", headers)
@@ -118,7 +97,9 @@ class Japscan : ParsedHttpSource() {
     }
 
     override fun popularMangaNextPageSelector(): String? = null
+
     override fun popularMangaSelector() = "#top_mangas_week li > span"
+
     override fun popularMangaFromElement(element: Element): SManga {
         val manga = SManga.create()
         element.select("a").first().let {
@@ -151,7 +132,9 @@ class Japscan : ParsedHttpSource() {
     }
 
     override fun latestUpdatesNextPageSelector(): String? = null
+
     override fun latestUpdatesSelector() = "#chapters > div > h3.text-truncate"
+
     override fun latestUpdatesFromElement(element: Element): SManga = popularMangaFromElement(element)
 
     // Search
@@ -178,7 +161,9 @@ class Japscan : ParsedHttpSource() {
     }
 
     override fun searchMangaNextPageSelector(): String? = "li.page-item:last-child:not(li.active)"
+
     override fun searchMangaSelector(): String = "div.card div.p-2, a.result-link"
+
     override fun searchMangaParse(response: Response): MangasPage {
         if ("live-search" in response.request().url().toString()) {
             val body = response.body()!!.string()
@@ -269,52 +254,20 @@ class Japscan : ParsedHttpSource() {
         }
     }
 
-    private fun createKeysheet(doc: Document, doc2: Document) {
-        val pageUrls = mutableListOf<String>()
-
-        var pages = doc.select("select#pages").first()?.select("option")!! // if this is null we're done
-        for (i in listOf(1, 3, 13, 28)) {
-            pageUrls.add(pages[i].attr("data-img").substring("https://c.japscan.co/".length, pages[i].attr("data-img").length - 4))
-        }
-        pages = doc2.select("select#pages").first()?.select("option")!! // if this is null we're done
-        for (i in listOf(0)) {
-            pageUrls.add(pages[i].attr("data-img").substring("https://c.japscan.co/".length, pages[i].attr("data-img").length - 4))
-        }
-
-        val az = "0123456789abcdefghijklmnopqrstuvwxyz".toCharArray()
-        val ks = "0123456789abcdefghijklmnopqrstuvwxyz".toCharArray()
-
-        for (i in 0 until realPageUrls.count())
-            for (j in realPageUrls[i].indices) {
-                if (realPageUrls[i][j] != pageUrls[i][j]) {
-                    ks[az.indexOf(pageUrls[i][j])] = realPageUrls[i][j]
-                }
-            }
-        keysheet = ks.joinToString("")
-    }
-
     override fun pageListParse(document: Document): List<Page> {
-        loadKeysheetChapters()
         val pages = mutableListOf<Page>()
         var imagePath = "(.*\\/).*".toRegex().find(document.select("#image").attr("data-src"))?.groupValues?.get(1)
         val imageScrambled = if (!document.select("script[src^='/js/iYFbYi_U']").isNullOrEmpty()) "&decodeImage" else ""
+        val script = "<script src=\"\\/zjs\\/(.+?)\\.".toRegex().find(document.html())?.groupValues?.get(1)
+        generateDictionary(script)
 
         document.select("select#pages").first()?.select("option")?.forEach {
             if (it.attr("data-img").startsWith("http")) imagePath = ""
-            pages.add(Page(pages.size, "", decodeImageUrl("$imagePath${it.attr("data-img")}") + imageScrambled))
+            val imgUrl = "$imagePath${it.attr("data-img")}"
+            val decodedUrl: String = decodeImgUrl(imgUrl)
+            pages.add(Page(pages.size, "", decodedUrl + imageScrambled))
         }
-
         return pages
-    }
-
-    private fun decodeImageUrl(url: String): String {
-        val az = "0123456789abcdefghijklmnopqrstuvwxyz"
-        // skip https://, cut after next slash and before extension
-        val urlBase = url.substring(0, url.indexOf('/', 10) + 1)
-        val extension = url.substring(url.length - 4, url.length)
-        val encodedPart = url.substring(url.indexOf('/', 10) + 1, url.length - 4)
-
-        return urlBase + encodedPart.map { if (az.indexOf(it) < 0) it else keysheet[az.indexOf(it)] }.joinToString("") + extension
     }
 
     override fun imageUrlParse(document: Document): String = ""
@@ -388,4 +341,67 @@ class Japscan : ParsedHttpSource() {
     }
 
     private var pageNumberDoc: Document? = null
+
+    private fun decodeImgUrl(fakeURL: String): String {
+        val point = fakeURL.indexOf("/", 9)
+        val origin = fakeURL.substring(0, point)
+        val path = fakeURL.substring(point, fakeURL.lastIndexOf("."))
+        val ext = fakeURL.substring(fakeURL.lastIndexOf("."))
+        val sb = java.lang.StringBuilder(origin)
+        for (s in path.split("".toRegex()).toTypedArray()) {
+            if (!s.isEmpty()) {
+                sb.append(dicc[s])
+            }
+        }
+        sb.append(ext)
+        return sb.toString()
+    }
+
+    @Synchronized
+    private fun generateDictionary(newDicName: String?) {
+        if (!currentScripts.contains(newDicName)) {
+            val data = client.newCall(Request.Builder().url("https://raw.githubusercontent.com/raulhaag/MiMangaNu/master/js_plugin/28.json").build()).execute().body()?.string()
+            val `object` = JSONObject(data)
+            val idxs: ArrayList<Int> = ArrayList()
+            val values: ArrayList<String> = ArrayList()
+            val sources: ArrayList<String> = ArrayList()
+            var array: JSONArray = `object`.getJSONArray("idxs")
+            for (i in 0 until array.length()) {
+                idxs.add(array.getInt(i))
+            }
+            array = `object`.getJSONArray("values")
+            for (i in 0 until array.length()) {
+                values.add(array.getString(i))
+            }
+            array = `object`.getJSONArray("pages")
+            for (i in 0 until array.length()) {
+                sources.add(array.getString(i))
+            }
+            val cl: Int = `object`.getInt("length")
+            val enc = StringBuilder()
+            dicc.clear()
+            currentScripts.clear()
+            for (web in sources) {
+                val data = client.newCall(Request.Builder().url(web).build()).execute().body()?.string()
+                if (data != null) {
+                    val id = "<script src=\"\\/zjs\\/(.+?)\\.".toRegex().find(data)?.groupValues?.get(1)
+                    if (id != null && !currentScripts.contains(id)) {
+                        currentScripts.add(id)
+                    }
+                    val ap = "<div id=\"image\" data-src=\"https://c.japscan.co(/.+?)\\..{3,4}\"".toRegex().find(data)?.groupValues?.get(1)
+                    enc.append(ap)
+                }
+            }
+            if (currentScripts.size != 3) {
+                throw Exception("Error creating dictionary (ids error, maybe hour change?)")
+            }
+            if (enc.length == cl) {
+                for (i in 0 until idxs.size) {
+                    dicc["" + enc[idxs[i]]] = values[i]
+                }
+            } else {
+                throw Exception("Error creating dictionary (not valid length)")
+            }
+        }
+    }
 }
